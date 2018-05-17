6ad3c4c5aeeb23e364cfc238b9e899be4f482cf0
niraj <niraj2pal@gmail.com>
2018-05-17 11:08:30 +0530

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180516074358) do
+ActiveRecord::Schema.define(version: 20180517053401) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(version: 20180516074358) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "mobile"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
