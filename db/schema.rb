f2f91767e3c26d4fb71b9a4489183b0a5bb54861
niraj <niraj2pal@gmail.com>
2018-05-17 11:22:18 +0530

    add user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180517053401) do
+ActiveRecord::Schema.define(version: 20180517054954) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "post_id"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180517053401) do
     t.text     "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
